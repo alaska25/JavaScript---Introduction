488e8c3cb2af97a6cc45610c4683ea6242c1f595
Arman <villegasarman23@gmail.com>
2022-05-26 10:22:38 -0700

    Added new code

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
-let firstName = "John";
-console.log("First Name: " + firstName);
-let lastName = "Smith";
-console.log("Last Name: " + lastName);
-let Age = "30";
-console.log("Age: " + Age);
+console.log("Hello World!");
+let firstName = "John", lastName = "Smith";
+let age = "30";
+let hobbies = ["Biking", "Mountain Climbing", "Swimming"];
+let workAddress = {houseNumber:'32', street: 'Washington', city: 'Lincoln', state: 'Nebraska'};
 
-console.log("Hobbies:");
-let Hobbies = ["Biking", "Mountain Climbing", "Swimming"]
-console.log(Hobbies);
+function printUserInfo(){
 
-console.log("Work Address:");
-let workAddress = {houseNumber:'32', street: 'Washington', city: 'Lincoln', state: 'Nebraska'}
-console.log(workAddress);
-console.log("John Smith is 30 years of age.");
+	console.log("Firstname: " +firstName);
+	console.log("Lastname: " +lastName);
+	console.log("Age: " +age);
+	console.log("Hobbies:");
+	console.log(hobbies);
+	console.log("WorkAddress:");
+	console.log(workAddress);
+}
+printUserInfo(firstName, lastName, age);
+
+function returnFunction(){
+	return firstName + lastName + age + hobbies + workAddress;
+}
+
+console.log(firstName+ " " + lastName + " is " + age + " years of age.");
 console.log("This was printed inside of the function");
-let Hobbie = ["Biking", "Mountain Climbing", "Swimming"]
-console.log(Hobbie);
+
+console.log(hobbies);
 console.log("This was printed inside of the function");
-let name = {houseNumber:"32", street: "Washington", city: "Lincoln", state: "Nebraska"}
-console.log(name);
 
 let isMarried = true;
-console.log("The value of isMarried is: " + isMarried);
+console.log("The value of isMarried is:" +isMarried);
+
