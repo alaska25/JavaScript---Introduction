7d557b003058d2be8ed845e90e1524a567899460
Arman <villegasarman23@gmail.com>
2022-05-26 05:19:06 -0700

    Added new code

---
+++ index.js
@@ -0,0 +1,24 @@
+let firstName = "John";
+console.log("First Name: " + firstName);
+let lastName = "Smith";
+console.log("Last Name: " + lastName);
+let Age = "30";
+console.log("Age: " + Age);
+
+console.log("Hobbies:");
+let Hobbies = ["Biking", "Mountain Climbing", "Swimming"]
+console.log(Hobbies);
+
+console.log("Work Address:");
+let workAddress = {houseNumber:'32', street: 'Washington', city: 'Lincoln', state: 'Nebraska'}
+console.log(workAddress);
+console.log("John Smith is 30 years of age.");
+console.log("This was printed inside of the function");
+let Hobbie = ["Biking", "Mountain Climbing", "Swimming"]
+console.log(Hobbie);
+console.log("This was printed inside of the function");
+let name = {houseNumber:"32", street: "Washington", city: "Lincoln", state: "Nebraska"}
+console.log(name);
+
+let isMarried = true;
+console.log("The value of isMarried is: " + isMarried);
